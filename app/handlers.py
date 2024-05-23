43d01d7141eee3374c206bcfba890146eb0c91ac
sergeesha <epifanovatany2005@gmail.com>
2024-05-23 16:02:00 +0300

    добавила функцию /register

--- app/handlers.py
+++ app/handlers.py
@@ -1,12 +1,17 @@
 from aiogram import F, Router
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
 
 router = Router()
 
-
+class Register(StatesGroup):
+    name = State()
+    age = State()
+    number = State()
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer(f'Добро пожаловать,{message.from_user.first_name} !')
@@ -37,4 +42,28 @@ async def coffee(callback: CallbackQuery):
 async def coffee(callback: CallbackQuery):
     await callback.answer('Вы выбрали категорию')
     await callback.message.answer('Вы выбрали категорию выпечка')
+
+@router.message(Command('register'))
+async def register(message: Message, state: FSMContext):
+    await state.set_state(Register.name)
+    await message.answer('Введите ваше имя')
+
+@router.message(Register.name)
+async def register_name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Register.age)
+    await message.answer('Введите ваш возраст')
+
+@router.message(Register.age)
+async def register_age(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await state.set_state(Register.number)
+    await message.answer('Отправьте ваш номер телефона', reply_markup=kb.get_number)
+
+@router.message(Register.number, F.contact)
+async def register_number(message: Message, state: FSMContext):
+    await state.update_data(number=message.contact.phone_number)
+    data=await state.get_data()
+    await message.answer(f'Ваше имя: {data["name"]}\nВаш возраст: {data["age"]}\nВаше номер телефона: {data["number"]}\n')
+    await state.clear()
 
--- app/keyboards.py
+++ app/keyboards.py
@@ -13,3 +13,7 @@ menu = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Кофе', callback_data='coffee')],
     [InlineKeyboardButton(text='Не кофе', callback_data='not coffee')],
     [InlineKeyboardButton(text='Выпечка', callback_data='bakery')]])
+
+get_number= ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Отправь номер',
+                                                        request_contact=True)]],
+                               resize_keyboard=True)

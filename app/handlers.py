96a90e5c092f73db038382fd52639eaa769a9f64
itkotik <vip.okaterina@gmail.com>
2024-05-28 22:36:16 +0300

    сделала все кнопки рабочими, добавила новый функционал

--- app/handlers.py
+++ app/handlers.py
@@ -1,9 +1,9 @@
 from aiogram import F, Router, Dispatcher, types
+from aiogram.enums import ChatAction
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
-
 import app.keyboards as kb
 from sqlite.database import db_start, register_or_update_user
 
@@ -13,6 +13,8 @@ class Register(StatesGroup):
     name = State()
     age = State()
     phone_number = State()
+
+COFFEE_PHOTO_PATH = "./prices/list1.jpg"
 
 @router.message(CommandStart())
 async def cmd_start(message: types.Message):
@@ -32,6 +34,21 @@ async def cmd_help(message: types.Message):
 async def cmd_catalog(message: types.Message):
     await message.answer(f'Выберите интересующий вас раздел 👇', reply_markup=kb.main)
 
+@router.message(F.text == 'О нас')
+async def about(message: types.Message):
+    await message.answer('Кофейня-пекарня "Перемена')
+
+@router.message(F.text == 'Контакты')
+async def about(message: types.Message):
+    await message.answer('Наши контакты!!!')
+
+@router.message(F.text == 'Бонусы')
+async def bonus(message: types.Message):
+    await message.answer('Здесь мы будем показывать ваши бонусы!')
+
+@router.message(F.text == 'Сайт')
+async def site(message: types.Message):
+    await message.answer('Здесь будет ссылка на наш сайт!')
 @router.message(F.text == 'Меню')
 async def menu(message: types.Message):
     await message.answer('Сделайте выбор: ', reply_markup=kb.menu)
@@ -39,17 +56,54 @@ async def menu(message: types.Message):
 @router.callback_query(F.data == 'coffee')
 async def coffee_callback(callback: types.CallbackQuery):
     await callback.answer('Вы выбрали категорию')
-    await callback.message.answer('Вы выбрали категорию кофе')
+    await coffee_command(callback.message)
+async def coffee_command(message: types.Message):
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id,
+        action=ChatAction.UPLOAD_PHOTO,
+    )
+    file_path = "./prices/menu.jpg"
+    await message.reply_photo(
+        photo=types.FSInputFile(path=file_path),
+        caption="Кофейные напитки",
+    )
+
+
 
 @router.callback_query(F.data == 'not coffee')
 async def not_coffee_callback(callback: types.CallbackQuery):
     await callback.answer('Вы выбрали категорию')
-    await callback.message.answer('Вы выбрали категорию не кофе')
+    await not_coffee_command(callback.message)
 
+
+async def not_coffee_command(message: types.Message):
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id,
+        action=ChatAction.UPLOAD_PHOTO,
+    )
+    file_path = "./prices/menu.jpg"
+    # await message.bot.send_photo()
+    await message.reply_photo(
+        photo=types.FSInputFile(path=file_path),
+        caption="Напитки",
+    )
 @router.callback_query(F.data == 'bakery')
 async def bakery_callback(callback: types.CallbackQuery):
     await callback.answer('Вы выбрали категорию')
-    await callback.message.answer('Вы выбрали категорию выпечка')
+    await bakery_command(callback.message)
+
+
+async def bakery_command(message: types.Message):
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id,
+        action=ChatAction.UPLOAD_PHOTO,
+    )
+    file_path = "./prices/menu.jpg"
+    # await message.bot.send_photo()
+    await message.reply_photo(
+        photo=types.FSInputFile(path=file_path),
+        caption="Выпечка",
+    )
 
 @router.message(Command('register'))
 async def register(message: types.Message, state: FSMContext):
@@ -76,3 +130,16 @@ async def register_number(message: Message, state: FSMContext):
     await register_or_update_user(user_id=message.from_user.id, name=data['name'], age=data['age'], phone_number=data['phone_number'])
     await message.reply('Вы успешно зарегистрировались!')
     await state.clear()
+
+@router.message(Command("pic"))
+async def handle_command_pic(message: types.Message):
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id,
+        action=ChatAction.UPLOAD_PHOTO,
+    )
+    file_path = "./prices/list1.png"
+    # await message.bot.send_photo()
+    await message.reply_photo(
+        photo=types.FSInputFile(path=file_path),
+        caption="Кофейные напитки",
+    )
